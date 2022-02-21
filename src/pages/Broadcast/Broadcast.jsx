4c72bdc1c34ecbd295bb13e3377cbad978930476
Dheeraj Patel <dpatel@gammastack.com>
2022-02-21 18:56:44 +0530

    refactor: added turn server

--- src/pages/Broadcast/Broadcast.jsx
+++ src/pages/Broadcast/Broadcast.jsx
@@ -8,10 +8,22 @@ let broadcastStream = null;
 let rtcPeerConnection = {};
 const iceServers = {
   iceServers: [
-    { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
-    { urls: "stun:stun3.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:19302" },
+    { urls: "stun:stun.l.google.com:19302" },
+    {
+      url: "turn:numb.viagenie.ca",
+      credential: "muazkh",
+      username: "webrtc@live.com",
+    },
+    {
+      url: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      url: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    }
   ],
 };
 const channelList = [
